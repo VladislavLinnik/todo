2a67122c306cce56829b93c0606201b9d6cfdf29
Vlad Linnik <vladislav.linnik.1999@gmail.com>
2019-02-28 11:18:07 +0200

    Started html-markup and minimal interactive with JS

--- js/index.js
+++ js/index.js
@@ -1,2 +1,30 @@
 'use strict';
 
+window.onload = function(){
+    document.querySelector(".js-input").focus();
+}
+
+var input = document.querySelector(".js-input"),
+    btn = document.querySelector(".js-btnAdd"),
+    btnDelete = document.querySelector(".js-btnDelete");
+
+input.addEventListener('blur', inputAdd);
+btnDelete.addEventListener('click', deleteTask);
+
+function deleteTask(){
+    alert(1);
+}
+function inputAdd(){
+    var val = input.value;
+    val.trim();
+
+    if (val != "" && val != " ") {
+        var elem = `
+        <li class="tasks-item">
+            <div class="task-text">${val}</div>
+            <div class="task-delete js-btnDelete">&#10008;</div>
+        </li>`;
+        document.querySelector('.tasks-list').insertAdjacentHTML('afterBegin', elem);
+        input.value = "";
+    }
+}
